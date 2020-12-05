ffa46f3dbb933a07c7a08e34b3cefe1f418ddf65
Andrew Dauphinais <idollabschicago@gmail.com>
2020-12-05 16:03:21 -0600

    Evets, Import, Export

---
+++ src/books.js
@@ -0,0 +1,24 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg',
+    title: 'If Animals Kissed Good Night',
+    author: 'Ann Whitford Paul',
+  },
+
+  {
+    id: 2,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71c1LRLBTBL._AC_UL200_SR200,200_.jpg',
+    title: 'Dog Man: Grime and Punishment',
+    author: 'Dav Pilkey',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL200_SR200,200_.jpg',
+    title: 'Becoming',
+    author: 'Michelle Obama',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -6,40 +6,25 @@ import ReactDom from 'react-dom';
 //CSS
 import './index.css';
 
-const books = [
-  {
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg',
-    title: 'If Animals Kissed Good Night',
-    author: 'Ann Whitford Paul',
-  },
+// const names = ['john', 'peter', 'susan'];
+// const newName = names.map((name) => {
+//   return <h1>{name}</h1>;
+// });
+// console.log(newName);
 
-  {
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/71c1LRLBTBL._AC_UL200_SR200,200_.jpg',
-    title: 'Dog Man: Grime and Punishment',
-    author: 'Dav Pilkey',
-  },
-];
+import { books } from './books';
+import Book from './Book';
+import { greeting } from './testing/testing';
 
-const names = ['john', 'peter', 'susan'];
-const newName = names.map((name) => {
-  return <h1>{name}</h1>;
-});
-console.log(newName);
 function BookList() {
-  return <section className='booklist'>{newName}</section>;
-}
-
-const Book = ({ img, title, author }) => {
-  //const { img, title, author } = props;
+  console.log(greeting);
   return (
-    <article className='book'>
-      <img src={img} alt='' />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
   );
-};
+}
 
 ReactDom.render(<BookList />, document.getElementById('root'));
